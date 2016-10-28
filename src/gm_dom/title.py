cb806c8c4642a7ea815349d0859b800be0bda38e
Mark Servilla <mark.servilla@gmail.com>
2016-10-27 20:28:23 -0600

    Add skeleton files for the generic metadata model.
    Each class file describes on key concept of metadata.

---
+++ src/gm_dom/title.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: title
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    10/21/16
+"""
+
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d% H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('title')
+
+
+import json
+
+class Title(object):
+
+    def __init__(self, title=None):
+        self.title = json.load(j )
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()

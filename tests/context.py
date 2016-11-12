cbf8efe5fb4af8cb23e080481bba8b944af7acaa
Mark Servilla <mark.servilla@gmail.com>
2016-11-12 15:14:14 -0700

    Moved module gm_dom to top-level and delete src directory

---
+++ tests/context.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: context
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    11/12/16
+"""
+
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d% H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('context')
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+import gm_dom
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()

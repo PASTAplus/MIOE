e01a888e004883c609639e7108b3728f15b37f24
Mark Servilla <mark.servilla@gmail.com>
2016-11-12 18:54:50 -0700

    Rename gm_doc to gm_dom

---
+++ gm_dom/gm_dom.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: gm_dom
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    10/21/16
+"""
+
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d% H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('gm_dom')
+
+from gm_dom import title
+
+class Gm_Dom(object):
+
+    def __init__(self):
+        self.title = title.Title()
+
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()

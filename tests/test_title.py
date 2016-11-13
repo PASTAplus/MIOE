e7b8b7a16204975502821291caed987ee8bbb76e
Mark Servilla <mark.servilla@gmail.com>
2016-11-12 20:48:38 -0700

    Add tests for title constructor and to_json

--- tests/test_title.py
+++ tests/test_title.py
@@ -20,16 +20,30 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 logging.getLogger('').setLevel(logging.WARN)
 logger = logging.getLogger('test_title')
 
+import json
 
 from .context import title
 
 
 class TestTitle(unittest.TestCase):
+    test_title = "This is a test title!"
+    json_title = json.dumps({'title': test_title})
+
     def setUp(self):
-        pass
+        self.title = title.Title(TestTitle.test_title)
 
     def tearDown(self):
         pass
+
+    def test_default_title(self):
+        self.assertEquals(TestTitle.test_title, self.title.get_title(),
+                          msg='{test}: expected title "{title}", but received "{error}"'.format(
+                              test='test_title', title=TestTitle.test_title,
+                              error=self.title.get_title()))
+
+    def test_to_json(self):
+        json_title = self.title.to_json()
+        self.assertEquals(TestTitle.json_title, json_title)
 
 
 if __name__ == '__main__':

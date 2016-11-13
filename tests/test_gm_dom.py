456b85854e8d84af5f2e56d0b62770607f1b1039
Mark Servilla <mark.servilla@gmail.com>
2016-11-12 18:58:10 -0700

    Rename from test_create_gm_doc to test_gm_dom

---
+++ tests/test_gm_dom.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: test_gm_dom
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    10/21/16
+"""
+
+import logging
+import unittest
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('test_gm_dom')
+
+from .context import gm_dom
+
+class TestGmDom(unittest.TestCase):
+
+    def setUp(self):
+        self.gm_dom = gm_dom.Gm_Dom()
+
+
+    def tearDown(self):
+        pass
+
+    def test_create(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

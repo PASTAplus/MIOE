fdab74bd582a1f8a0ba0ce256c45b7f2bff7eae5
Mark Servilla <mark.servilla@gmail.com>
2016-11-23 13:57:57 -0700

    Add dataset component to model

---
+++ gm_dom/dataset.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: dataset
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    11/23/16
+"""
+
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d% H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('dataset')
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
